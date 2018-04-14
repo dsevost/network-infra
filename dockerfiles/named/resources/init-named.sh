64db6253336d510250180a82e01ee08217bbdfe7
dmitry sevostyanov <dmitry.sevostyanov@gmail.com>
2018-04-14 13:49:00 +0300

    zone files added

--- dockerfiles/named/resources/init-named.sh
+++ dockerfiles/named/resources/init-named.sh
@@ -15,7 +15,7 @@ function str2list() {
 }
 
 named_etc_ro=${NAMED_ETC}_ro
-#named_etc_ro=${NAMED_ETC}
+named_etc_ro=${NAMED_ETC}
 
 cat ${named_etc_ro}/acl.conf.${tmpl_suffix} | sed "s|__ACL_LOCAL_NETS__|$(str2list ACL_LOCAL_NETS)|" > ${NAMED_ETC}/acl.conf
 
@@ -33,3 +33,13 @@ esac
 cat ${named_etc_ro}/named.conf.${tmpl_suffix} | sed "s|__ALLOW_RECURSION__|$recursion|" > ${NAMED_ETC}/named.conf
 
 mkdir -p ${NAMED_DATA}/{data,dynamic,forward,reverse}
+
+master_in_addr_arpa=`echo $LOCAL_ZONE_FORWARD_NET | sed 's/\([0-9]\+\)\.\([0-9]\+\)\.\([0-9]\+\)/\3.\2.\1/'`
+cat ${named_etc_ro}/local-zone.conf.${tmpl_suffix} | sed "\
+    s|__MASTER_LOCAL_ZONE_NAME__|$MASTER_LOCAL_ZONE_NAME| ; \
+    s|__MASTER_IN_ADDR_ARPA__|$master_in_addr_arpa| ; \
+    " > ${NAMED_ETC}/local-zone.conf
+
+cat ${named_etc_ro}/local.db.${tmpl_suffix} | sed "s|__LOCAL_ZONE_FORWARD_NET__|$LOCAL_ZONE_FORWARD_NET|" > ${NAMED_DATA}/forward/${MASTER_LOCAL_ZONE_NAME}.db
+
+cat ${named_etc_ro}/reverse.db.${tmpl_suffix} | sed "s|__MASTER_LOCAL_ZONE_NAME__|$MASTER_LOCAL_ZONE_NAME|" > ${NAMED_DATA}/reverse/$master_in_addr_arpa.db
